17c0c0fb4cfca2081bd6575347a9bcfc1238e074
AdvancedHuscky <menzongwei@126.com>
2018-04-26 14:10:07 +0800

    demo01 get post

--- demo01.js
+++ demo01.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let queryString = require('querystring')
 
 let app = express();
 app.listen(3000);
@@ -10,4 +11,20 @@ app.get('/test01',(req,res)=>{
 })
 app.get('/testcss',(req,res)=>{
   res.sendFile(__dirname+"/static/test.css")
+})
+app.get('/jq',(req,res)=>{
+  res.sendFile(__dirname+"/static/jquery-1.11.3.js")
+})
+app.get('/detail',(req,res)=>{
+  res.send(`this is ${req.query.id}的详情页`);
+})
+app.post('/login',(req,rep)=>{
+  //post 不能按照req.query解析所传来的参数
+  // 方式1：body-parser
+  // 方式2：事件
+  req.on('data',(buf)=>{
+    console.log(buf.toString());
+    
+  })
+
 })
