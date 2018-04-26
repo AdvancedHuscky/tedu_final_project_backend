b13e9355ef1ee7c5d6292355870209b9dddec7e2
hahaha <hahaha@gmail.com>
2018-04-26 11:22:30 +0800

    demo01

--- demo01.js
+++ demo01.js
@@ -5,3 +5,9 @@ app.listen(3000);
 app.get('/',(req,res)=>{
   res.send('Hello World!')
 })
+app.get('/test01',(req,res)=>{
+  res.sendFile(__dirname+"/static/test01.html");
+})
+app.get('/testcss',(req,res)=>{
+  res.sendFile(__dirname+"/static/test.css")
+})
